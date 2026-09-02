a6cf6cd68371036e5676c755754c9dbb89719d16
Hesam Sormeyli <hesam1385hs@gmail.com>
2026-09-02 22:31:06 +0330

    0.4
    task delete button 1

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 const addTaskButton = document.querySelector('#newTask button');
 const input = document.querySelector('#newTask input');
 const tasksContainer = document.querySelector('.tasksContainer');
+// const deleteButtonDOM = document.querySelector('')
 
-addTaskButton.addEventListener("click", ((e) => {
+
+
+
+addTaskButton.addEventListener("click", addTask);
+input.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") { addTask() }
+})
+function addTask() {
     p = document.createElement('p');
     p.textContent = input.value;
     p.classList.add('tasks');
+    deleteButton = document.createElement('button');
+    deleteButton.classList.add('tasks');
+    deleteButton.classList.add('deleteButton')
+    deleteButton.textContent = "X";
+    deleteButton.addEventListener("click", (e) => {
+        tasksContainer.remove('p');
+    });
+    p.appendChild(deleteButton);
     tasksContainer.appendChild(p);
-    input.value = ""
-    
-}))
+    input.value = "";
+}
+
+// function deleteTask(){
+//     tasksContainer.removeChild('p');
+// }
