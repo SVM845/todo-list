c05a45b076450d7601860a1d4ee9fb3a589a713f
Hesam Sormeyli <hesam1385hs@gmail.com>
2026-09-03 15:33:48 +0330

    0.6
    empty input handling 1

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const addTaskButton = document.querySelector('#newTask button');
 const input = document.querySelector('#newTask input');
 const tasksContainer = document.querySelector('.tasksContainer');
+
 // const deleteButtonDOM = document.querySelector('')
 
 
@@ -11,19 +12,39 @@ input.addEventListener("keydown", (e) => {
     if (e.key === "Enter") { addTask() }
 })
 function addTask() {
-    const p = document.createElement('p');
-    p.textContent = input.value;
-    p.classList.add('tasks');
-    const deleteButton = document.createElement('button');
-    deleteButton.classList.add('tasks');
-    deleteButton.classList.add('deleteButton')
-    deleteButton.textContent = "X";
-    deleteButton.addEventListener("click", (e) => {
-        tasksContainer.removeChild(e.target.parentElement)
-    });
-    p.appendChild(deleteButton);
-    tasksContainer.appendChild(p);
-    input.value = "";
+    if (input.value == "") {}
+    else {
+        const taskButtons = document.createElement('div');
+        const doneButton = document.createElement('button');
+        const deleteButton = document.createElement('button');
+        const p = document.createElement('p');
+
+        taskButtons.classList.add('taskButtons');
+
+        p.textContent = input.value;
+        p.classList.add('tasks');
+        doneButton.classList.add('doneButton')
+        doneButton.textContent = "✔";
+        doneButton.classList.add('taskButtons')
+        taskButtons.appendChild(doneButton);
+
+
+        deleteButton.classList.add('tasks');
+        deleteButton.classList.add('deleteButton')
+        deleteButton.textContent = "X";
+
+        deleteButton.addEventListener("click", (e) => {
+            tasksContainer.removeChild(e.target.parentElement.parentElement)
+        });
+
+        deleteButton.classList.add('taskButtons');
+        taskButtons.appendChild(deleteButton);
+
+        p.appendChild(taskButtons);
+        tasksContainer.appendChild(p);
+        input.value = "";
+    }
+
 }
 
 // function deleteTask(){
